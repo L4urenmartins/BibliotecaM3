20219289f31fd3f8a7c3354417e05aaae2b73bfe
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-08-14 19:17:26 -0300

    Update do projeto

--- SQL/biblioteca.sql
+++ SQL/biblioteca.sql
@@ -1,38 +1,36 @@
--- criação o banco de dados da biblioteca --
+-- Criação do banco de dados da biblioteca --
 CREATE DATABASE IF NOT EXISTS Biblioteca;
 USE Biblioteca;
 
--- criação da tabela "Editora" --
-CREATE TABLE  IF NOT EXISTS Editoras (
+-- Criação da tabela "Editora" --
+CREATE TABLE IF NOT EXISTS Editora (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100),
+    Nome VARCHAR(100) NOT NULL,
     Localizacao VARCHAR(100)
 );
 
---criação da tabela "Categorias" --
-CREATE TABLE IF NOT EXISTS Categorias (
+-- Criação da tabela "Categoria" --
+CREATE TABLE IF NOT EXISTS Categoria (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50)
+    Nome VARCHAR(50) NOT NULL
 );
 
---criação da tabela "Autores" --
-CREATE TABLE IF NOT EXISTS Autores (
+-- Criação da tabela "Autor" --
+CREATE TABLE IF NOT EXISTS Autor (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100),
+    Nome VARCHAR(100) NOT NULL,
     Pais VARCHAR(50)
 );
 
---criação da tabela "Livos" --
-CREATE TABLE IF NOT EXISTS Livros (
+-- Criação da tabela "Livro" --
+CREATE TABLE IF NOT EXISTS Livro (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Titulo VARCHAR(200),
-    AnoPublicacao INT,
+    Titulo VARCHAR(200) NOT NULL,
+    AnoPublicacao YEAR,
     EditoraID INT,
     CategoriaID INT,
     AutorID INT,
-    FOREIGN KEY (EditoraID) REFERENCES Editoras(ID),
-    FOREIGN KEY (CategoriaID) REFERENCES Categorias(ID),
-    FOREIGN KEY (AutorID) REFERENCES Autores(ID)
+    FOREIGN KEY (EditoraID) REFERENCES Editora(ID),
+    FOREIGN KEY (CategoriaID) REFERENCES Categoria(ID),
+    FOREIGN KEY (AutorID) REFERENCES Autor(ID)
 );
-
-
--- SQL/populando_tabelas2.sql
+++ SQL/populando_tabelas2.sql
@@ -1,23 +1,23 @@
--- Inserindo as categorias --
+-- Inserção de dados na tabela "Categoria"
 INSERT INTO Categoria (Nome) VALUES
-('Fantasia'),
-('Aventura');
+    ('Fantasia'),
+    ('Aventura');
 
--- Inserindo o autor --
+-- Inserção de dados na tabela "Autores"
 INSERT INTO Autor (Nome, Pais) VALUES
-('George R. R. Martin', 'Estados Unidos');
+    ('George R. R. Martin', 'Estados Unidos');
 
--- Inserindo as editoras --
-INSERT INTO Editoras (Nome, Localizacao) VALUES
-('Editora Bantam Spectra', 'New York, Estados Unidos'),
-('Editora Harper Voyager ', 'London, Reino Unido');
+-- Inserção de dados na tabela "Editoras"
+INSERT INTO Editora (Nome, Localizacao) VALUES
+    ('Editora Bantam Spectra', 'New York, Estados Unidos'),
+    ('Editora Harper Voyager', 'London, Reino Unido');
 
--- Inserindo os livros das Crônicas de Gelo e Fogo e os derivados --
-INSERT INTO Livros (Titulo, AnoPublicacao, EditoraID, CategoriaID, AutorID) VALUES
-('A Game of Thrones', 1996, 1, 1, 1),
-('A Clash of Kings', 1998, 2, 1, 1),
-('A Storm of Swords', 2000, 1, 1, 1),
-('A Feast for Crows', 2005, 2, 1, 1),
-('A Dance with Dragons', 2011, 1, 1, 1),
-('The World of Ice & Fire', 2014, 2, 2, 1),
-('Fire and Blood', 2018, 1, 2, 1);
+-- Inserção de dados na tabela "Livros" das Crônicas de Gelo e Fogo e os derivados
+INSERT INTO Livro (Titulo, AnoPublicacao, EditoraID, CategoriaID, AutorID) VALUES
+    ('A Game of Thrones', 1996, 1, 1, 1),
+    ('A Clash of Kings', 1998, 2, 1, 1),
+    ('A Storm of Swords', 2000, 1, 1, 1),
+    ('A Feast for Crows', 2005, 2, 1, 1),
+    ('A Dance with Dragons', 2011, 1, 1, 1),
+    ('The World of Ice & Fire', 2014, 2, 2, 1),
+    ('Fire and Blood', 2018, 1, 2, 1);
